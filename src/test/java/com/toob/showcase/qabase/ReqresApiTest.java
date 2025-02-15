b4efdeffdd1a449cf9af02a3375b6d464e259df1
Thabo Matjuda <tl.matjuda@gmail.com>
2025-02-15 11:42:44 +0200

    1. Added more meat to the test sample

--- src/test/java/com/toob/showcase/qabase/ReqresApiTest.java
+++ src/test/java/com/toob/showcase/qabase/ReqresApiTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.qameta.allure.Allure.step;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -21,16 +22,24 @@ class ReqresApiTest extends AbstractHttpTest {
 
     @Test
     @Story("Fetch Single User")
-    void shouldGetSingleUser() {
+    void shouldGetSingleUserById() {
         final String id = "2";
 
         Response response = step( String.format("Fetch User By Id : %s", id), () -> {
-            Response internalResponse = RestClient.get(String.format("/api/user/%s", id));
-            assertNotNull(internalResponse);
-            return internalResponse;
+            Response resp = RestClient.get(String.format("/api/user/%s", id));
+            assertNotNull(resp);
+            return resp;
         });
 
         confirmStatusCode(200, response);
+
+        step("Verify specific response properties", () -> {
+            response.then().body("data.id", equalTo(2))
+                    .body("data.name", equalTo("fuchsia rose"))
+                    .body("data.year", equalTo(2001))
+                    .body("data.color", equalTo("#C74375"))
+                    .body("data.pantone_value", equalTo("17-2031"));
+        });
 
         step("Validate response contains at least one User record", () -> {
             response.then().body("size()", greaterThan(0));
